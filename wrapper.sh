6618417ba517c2c00caf9bf858e20f7d708c012c
marianburdianov <marian.burdianov@gmail.com>
2021-05-21 19:15:34 +0300

    add application-container.properties file

--- wrapper.sh
+++ wrapper.sh
@@ -2,7 +2,6 @@
 while ! exec 6<>/dev/tcp/"${DATABASE_HOST}"/"${DATABASE_PORT}"; do
     echo "Trying to connect to MySQL at ${DATABASE_HOST}:${DATABASE_PORT}..."
     sleep 10
-
 done
 echo ">> connected to MySQL database! <<"
 java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /opt/docker-jenkins-integration.jar
