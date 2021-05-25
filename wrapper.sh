c4237853905634703e486dbd9855501bb7066424
marianburdianov <marian.burdianov@gmail.com>
2021-05-25 11:07:11 +0300

    wrapper.sh uncomment line 8

--- wrapper.sh
+++ wrapper.sh
@@ -5,4 +5,4 @@ while ! exec 6<>/dev/tcp/"${DATABASE_HOST}"/"${DATABASE_PORT}"; do
 
 done
 echo "<<< connect to MySql database >>>"
-#java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /opt/docker-jenkins-integration.jar
+java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /opt/docker-jenkins-integration.jar
