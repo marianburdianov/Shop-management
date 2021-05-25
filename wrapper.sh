32893ca57fd4da13d56ca783dca381936724f71e
marianburdianov <marian.burdianov@gmail.com>
2021-05-25 11:23:16 +0300

    wrapper.sh change to previous state

--- wrapper.sh
+++ wrapper.sh
@@ -3,6 +3,6 @@ while ! exec 6<>/dev/tcp/"${DATABASE_HOST}"/"${DATABASE_PORT}"; do
     echo "Trying to connect to MySql at ${DATABASE_HOST}:${DATABASE_PORT}..."
     sleep 10
 
+done
 echo "<<< connect to MySql database >>>"
 java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /opt/docker-jenkins-integration.jar
-done
