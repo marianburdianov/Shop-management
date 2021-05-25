4f4bb253eae21ff2d65be728351b5fe00fff9804
marianburdianov <marian.burdianov@gmail.com>
2021-05-25 10:24:16 +0300

    recreate wrapper.sh file

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-while ! exec 6<>/dev/tcp/"${DATABASE_HOST}"/"${DATABASE_PORT}"; do
-    echo "Trying to connect to MySQL at ${DATABASE_HOST}:${DATABASE_PORT}..."
+#!bin/bash
+while ! exec 6<>/dev/tcp/${DATABASE_HOST}/${DATABASE_PORT}; do
+    echo "Trying to connect to MySql at ${DATABASE_HOST}:${DATABASE_PORT}..."
     sleep 10
+
 done
-echo ">> connected to MySQL database! <<"
+echo "<<< connect to MySql database >>>"
 java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /opt/docker-jenkins-integration.jar
