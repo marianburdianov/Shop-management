c8f80d0dc5f50275c9c4d01df6ce1b429096525f
marianburdianov <marian.burdianov@gmail.com>
2021-05-25 12:45:22 +0300

    wrapper.sh add another script for test

--- wrapper.sh
+++ wrapper.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-while ! exec 6<>/dev/tcp/"${DATABASE_HOST}"/"${DATABASE_PORT}"; do
+set -e
 
-    echo "Trying to connect to MySql at ${DATABASE_HOST}:${DATABASE_PORT}..."
-    sleep 10
+if [ "$1" = 'postgres' ]; then
+    chown -R postgres "$PGDATA"
 
-done
-echo "<<< connect to MySql database >>>"
-java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /opt/docker-jenkins-integration.jar
+    if [ -z "$(ls -A "$PGDATA")" ]; then
+        gosu postgres initdb
+    fi
+
+    exec gosu postgres "$@"
+fi
+
+exec "$@"
+##!/bin/bash
+#while ! exec 6<>/dev/tcp/"${DATABASE_HOST}"/"${DATABASE_PORT}"; do
+#
+#    echo "Trying to connect to MySql at ${DATABASE_HOST}:${DATABASE_PORT}..."
+#    sleep 10
+#
+#done
+#echo "<<< connect to MySql database >>>"
+#java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /opt/docker-jenkins-integration.jar
